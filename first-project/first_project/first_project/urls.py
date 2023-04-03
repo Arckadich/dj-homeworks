f0fc0fbd351707ed3bf6682d9bdf78e5c02286cd
Arckadich <kozlowva@gmail.com>
2023-04-03 17:02:48 +0300

    first commit

--- first-project/first_project/first_project/urls.py
+++ first-project/first_project/first_project/urls.py
@@ -18,12 +18,38 @@ from django.urls import path, include
 
 from app.views import home_view
 
+from django.urls import include, path
+from . import views
+from .views import HomeView
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    # Раскомментируйте код, чтобы данные урлы 
-    # обрабатывались Django
-    # path('current_time/', time_view, name='time'),
-    # path('workdir/', workdir_view, name='workdir'),
+    path('', views.home, name='home'),
+]
+# В этом примере шаблон URL '' (пустая строка) сопоставляется с домашней функцией в модуле view.
+
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+]
+# В этом примере шаблон URL '' (пустая строка) сопоставляется с представлением на основе класса HomeView.
+# Метод as_view() вызывается для создания экземпляра класса представления, который может обрабатывать запрос.
+
+
+
+urlpatterns = [
+    path('blog/', include('blog.urls')),
+]
+#В этом примере функция include() используется для включения конфигурации URL для приложения блога.
+# Любые URL-адреса, начинающиеся с blog/, будут обрабатываться URLconf в приложении блога.
+
+
+
+from django.urls import path
+from .views import time_view, workdir_view
+from django.contrib import admin
+
+urlpatterns = [
+    path('current_time/', time_view, name='time'),
+    path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
 ]
